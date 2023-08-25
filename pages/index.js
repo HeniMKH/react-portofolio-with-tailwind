ad83efd6eba5f88aee8289e7da94e4c21724fcad
Hengameh M.Khani <afsooneday@gmail.com>
2023-08-25 22:49:24 +0100

    nav done

--- pages/index.js
+++ pages/index.js
@@ -35,13 +35,13 @@ export default function Home() {
         <section className=" min-h-screen">
           <nav className="py-10 mb-12 flex justify-between">
             <h1 className="text-xl font-burtons">developedbyed</h1>
-            <ul className="flex item -center">
+            <ul className="flex items -center">
               <li>
                 <BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className="cursor-pointer text-2xl"/>
 
               </li>
               <li>
-                <a className="bg-gradient-to-r from-cyan-500 to-teal-500 px-4 py-2 rounded- md ml-8" href="#" >
+                <a className="bg-gradient-to-r from-cyan-500 to-teal-500 px-4 py-2 rounded-md ml-8" href="#" >
                 Resume</a></li>
 
 
